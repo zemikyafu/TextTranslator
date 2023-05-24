70157fa011a94e6389e048c6f064e744ec394b2f
yafu <zemikfcb@gmail.com>
2023-05-24 19:47:01 +0300

    credential removed

--- translateAWS.py
+++ translateAWS.py
@@ -2,8 +2,8 @@ import boto3
 
 class translateAWS:
       def translateWithAWS(fromLang, toLang,text):
-            aws_access_key_id = 'AKIAWUUKE34OPY3IQH2M'
-            aws_secret_access_key = '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'
+            aws_access_key_id = ''
+            aws_secret_access_key = ''
             region_name = 'us-east-1'  
             client = boto3.client('translate', aws_access_key_id=aws_access_key_id,
                                 aws_secret_access_key=aws_secret_access_key,
